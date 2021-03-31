adaa2820d9d605d9235d695e23e3aae66fa5166e
mikeyrf <mikeyrf@users.noreply.github.com>
2021-03-31 10:19:01 -0400

    Init master chef token

--- interfaces/IOVLToken.sol
+++ interfaces/IOVLToken.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-import {IDetailedERC20} from "./IDetailedERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IOVLToken is IDetailedERC20 {
+
+interface IOVLToken is IERC20 {
 
   function mint(address _recipient, uint256 _amount) external;
 
